3988425b8273b1890cd78cb40d80a71d9868e1ff
max-t-wheeler <13712788+max-t-wheeler@users.noreply.github.com>
2025-06-09 00:31:17 -0700

    add requirements.txt, update imports

--- vardamir/gcp_utils.py
+++ vardamir/gcp_utils.py
@@ -2,7 +2,7 @@ import os
 
 from google.colab import drive
 
-def mount_google_drive(file_path: str='/content/drive', remount=False):
+def mount_google_drive(file_path: str='/content/drive', remount: bool=False) -> None:
     if remount or os.environ.get('google_drive_mounted') is None:
         drive.mount(file_path)
         os.environ['google_drive_mounted'] = 'true'
