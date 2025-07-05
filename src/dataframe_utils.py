fcb8255f70ce02d8b61affaac79d78b3e60fd375
max-t-wheeler <13712788+max-t-wheeler@users.noreply.github.com>
2025-07-05 14:51:35 -0700

    update helper functions

--- src/dataframe_utils.py
+++ src/dataframe_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-def convert_to_date(data: pd.DataFrame, column: str) -> pd.Series:
-  data[column] = pd.to_datetime(data[column], errors='coerce').dt.date
+def convert_to_date(data: pd.DataFrame, column: str, date_format: str=None) -> pd.Series:
+  data[column] = pd.to_datetime(data[column], format=date_format, errors='coerce').dt.date
 
 def drop_empty_records(data: pd.DataFrame, subset: list=None) -> None:
   data.dropna(how='all', subset=subset, inplace=True)
--- src/file_utils.py
+++ src/file_utils.py
@@ -1,11 +1,24 @@
 from pathlib import PosixPath
 from zipfile import ZipFile
 
-def summarize_zip_contents(zip_file_path: PosixPath) -> None:
+def get_zip_content_summary(zip_file_path: PosixPath) -> pd.DataFrame:
+  zip_file_names = []
+  zip_file_sizes = []
+  zip_file_compress_sizes = []
+
   with ZipFile(zip_file_path) as zip_file:
     for zip_info in zip_file.infolist():
-      print(zip_info.filename)
-      print(f'File Size: {zip_info.file_size * 1e-6} MB')
-      print(f'Compressed File Size: {zip_info.compress_size * 1e-6} MB')
+      zip_file_names.append(zip_info.filename)
+      zip_file_sizes.append(zip_info.file_size)
+      zip_file_compress_sizes.append(zip_info.compress_size)
 
-      print()
+  dataframe = pd.DataFrame({
+      'file_name': zip_file_names,
+      'file_size': zip_file_sizes,
+      'file_compress_size': zip_file_compress_sizes
+  })
+
+  dataframe['file_size_mb'] = dataframe['file_size'].apply(lambda x: f'{x * 1e-6} MB')
+  dataframe['file_comress_size_mb'] = dataframe['file_compress_size'].apply(lambda x: f'{x * 1e-6} MB')
+
+  return dataframe
